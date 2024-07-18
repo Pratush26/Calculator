795ee58057e7faf86fbe636ef64a33df06605a44
root <root@localhost>
2024-07-18 19:28:23 +0600

    auto publish 1721309303266

--- js/main.js
+++ js/main.js
@@ -1,12 +1,38 @@
 // Add all your JS here
+const p = 22/7;
 let string = ""
+let output = document.getElementById("out");
 let buttons = document.querySelectorAll('.btn')
 
-console.log(Array.from(".btn"))
-/*Array.from('buttons').forEach((b)=>{
-  b.AddEventLisener('click',(e)=>{
-    if(e.target.InnerHTML =="="){
-      string = eval(string)
+Array.from(buttons).forEach((button)=>{
+  button.addEventListener("click",(e)=>{
+  if(e.target.innerHTML =="="){
+    string = eval(string);
+    output.value = string;
+  }
+  else if(e.target.innerHTML =="AC"){
+   string = "";
+   output.value = string;
+  }
+   else if(e.target.innerHTML =="pi"){
+   string = string + p;
+   output.value = string;
+  }
+   else if(e.target.innerHTML =="DEL"){
+   string = "";
+   output.value = string;
+  }
+   else if(e.target.innerHTML =="%"){
+   string = "";
+   output.value = string;
+  }
+   else if(e.target.innerHTML =="^"){
+   string = "";
+   output.value = string;
+  }
+  else{
+    string = string + e.target.innerHTML;
+    output.value = string;
     }
   })
-)}*/
+})
