8fde59272f068d4d5a685fbe786cbc5a0a413848
root <root@localhost>
2024-07-18 18:22:20 +0600

    auto publish 1721305340129

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// Add all your JS here
+let string = ""
+let buttons = document.querySelectorAll('.btn')
+
+console.log(Array.from(".btn"))
+/*Array.from('buttons').forEach((b)=>{
+  b.AddEventLisener('click',(e)=>{
+    if(e.target.InnerHTML =="="){
+      string = eval(string)
+    }
+  })
+)}*/
---
+++ js/mains.js
@@ -0,0 +1,8 @@
+let string = "5"
+let buttons = document.querySelectorAll(".btn")
+console.log(buttons)
+let c = document.getElementsByTagName("h1");
+c[0].innerHTML=" HI"
+//buttons.addEventListener("click",()=>{
+// document.querySelector(.h).innerHTML="cal"
+//})
